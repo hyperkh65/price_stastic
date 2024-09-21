209871af58f206cdd1b3a37975f8303c82dc25fd
hyperkh65 <136246416+hyperkh65@users.noreply.github.com>
2024-09-21 14:55:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ end_year_month = st.sidebar.text_input("조회 종료 년월 (YYYYMM 형식, 예
 data_query_button = st.sidebar.button("데이터 조회")
 
 # 애플 고딕 폰트 경로 설정
-font_path = '/System/Library/Fonts/Supplemental/AppleGothic.ttf'
+font_path = '/System/Library/Fonts/Supplemental/font/AppleGothic.ttf'
 plt.rcParams['font.family'] = 'AppleGothic'
 
 # 현재 날짜를 기준으로 기간 설정
