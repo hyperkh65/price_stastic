5831f993fdcee5d53d20b8776fd06a88c3a9570f
hyperkh65 <136246416+hyperkh65@users.noreply.github.com>
2024-09-21 17:57:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -185,8 +185,11 @@ if data_query_button:
         st.pyplot(plt)
 
         # 전용면적 범위 설정
-        bins = [0, 20, 40, 60, 80, 100, 120, 140, float('inf')]
-        labels = ['0-20', '20-40', '40-60', '60-80', '80-100', '100-120', '120-140', '140 이상']
+        selected_data['전용면적'] = selected_data['전용면적'].astype(float)  # 타입 변환
+        selected_data = selected_data[selected_data['전용면적'].notna()]  # NaN 값 제거
+
+        bins = [0, 80, 100, 120, 140, float('inf')]
+        labels = ['0-80', '80-100', '100-120', '120-140', '140 이상']
         selected_data['면적 범위'] = pd.cut(selected_data['전용면적'], bins=bins, labels=labels, right=False)
 
         # 면적 범위별 거래량 집계
